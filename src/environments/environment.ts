a2c2b7c92bbe75dcca714ea16f90eb8d3ba4635a
Estácio Pereira <estacio.pereira@gmail.com>
2018-05-27 16:49:37 -0300

    Create error module

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDeDD1_c-iPxPWAZUcdLZosZ3cv6nOWOwA",
-    authDomain: "angular-skeleton-pwa.firebaseapp.com",
-    databaseURL: "https://angular-skeleton-pwa.firebaseio.com",
-    projectId: "angular-skeleton-pwa",
-    storageBucket: "angular-skeleton-pwa.appspot.com",
-    messagingSenderId: "93000299685"
+    apiKey: 'AIzaSyDeDD1_c-iPxPWAZUcdLZosZ3cv6nOWOwA',
+    authDomain: 'angular-skeleton-pwa.firebaseapp.com',
+    databaseURL: 'https://angular-skeleton-pwa.firebaseio.com',
+    projectId: 'angular-skeleton-pwa',
+    storageBucket: 'angular-skeleton-pwa.appspot.com',
+    messagingSenderId: '93000299685',
   },
 };
 
