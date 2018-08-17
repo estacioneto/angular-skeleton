a6dfe56d50487dcb8af9d58bd78459d6b6f40d94
Estácio Pereira <estacio.pereira@gmail.com>
2018-08-17 09:25:24 -0300

    Add flex layout

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
@@ -9,6 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   exports: [
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatProgressBarModule,
     MatButtonModule,
     MatDividerModule,
